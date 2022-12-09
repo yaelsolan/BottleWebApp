641823129baeedb44ccc459508dcfeddd840485e
Oz Tiram <oz.tiram@gmail.com>
2022-12-09 19:02:31 +0100

    Move the app into a package

--- myapp/app.py
+++ myapp/app.py
@@ -1,23 +1,27 @@
-from bottle import route, run, template, get, post, request, error, response, static_file, abort, redirect
-@route('/')
+from bottle import Bottle, route, run, template, get, post, request, error, response, static_file, abort, redirect
+
+
+app = Bottle()
+
+@app.route('/')
 def index():
 	return "My first web app with bottle"
 	
-@route('/hello')
-@route('/hello/<name>')
+@app.route('/hello')
+@app.route('/hello/<name>')
 def hello(name="your_name"):
 	return template("Hello {{template_name}}", template_name=name)
 	
-@route('/wiki/Learn')
+@app.route('/wiki/Learn')
 def wiki_Learn():
 	return "Lets learn!"
 
-@route('/follow/yooli')
+@app.route('/follow/yooli')
 def follow_yooli():
 	return "Follow yooli"
 
 
-@get('/login') #create new route; request
+@app.get('/login') #create new route; request
 def login ():
     return '''
         <form action="/login" method="post">
@@ -27,7 +31,7 @@ def login ():
         </form>
     '''
 
-@post('/login') 
+@app.post('/login') 
 def check_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
@@ -36,48 +40,48 @@ def check_login():
     else:
         return "<p>Login failed.</p>"
 	
-@error(404)
+@app.error(404)
 def error404(error):
     return 'Nothing here, sorry'	
     
-@route('/iso')
+@app.route('/iso')
 def get_iso():
     response.charset = 'ISO-8859-15'
     return 'This will be sent with ISO-8859-15 encoding.'
 
-@route('/latin9')
+@app.route('/latin9')
 def get_latin():
     response.content_type = 'text/html; charset=latin9'
     return u'ISO-8859-15 is also known as latin9.'
 	
-@route('/images/<filename:re:.*\.jpeg>')
+@app.route('/images/<filename:re:.*\.jpeg>')
 def send_image(filename):
     return static_file(filename, root='/home/yael/bottle', mimetype='yool.jpeg')
 
-@route('/static/<filename:path>')
+@app.route('/static/<filename:path>')
 def send_static(filename):
     return static_file(filename, root='/home/yael/bottle')  
     
-@route('/download/<filename:path>')
+@app.route('/download/<filename:path>')
 def download(filename):
     return static_file(filename, root='/home/yael/bottle', download=filename)
     
-@route('/restricted')
+@app.route('/restricted')
 def restricted():
     	abort(401, "Sorry, access denied.") 
     
     
-@route('/forgeturl')
+@app.route('/forgeturl')
 def wrong():
     redirect("/therighturl")
     
-@route('/wiki/<page>')
+@app.route('/wiki/<page>')
 def wiki(page):
     response.set_header('Set-Cookie', 'name=value')
     response.add_header('Set-Cookie', 'name2=value2')
     return 'Hello'
     
-@route('/hello')
+@app.route('/hello')
 def hello_again():
     if request.get_cookie("visited"):
         return "Welcome back! Nice to see you again"
@@ -85,7 +89,7 @@ def hello_again():
         response.set_cookie("visited", "yes")
         return "Hello there! Nice to meet you"  
         
-@route('/login')
+@app.route('/login')
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
@@ -95,7 +99,7 @@ def do_login():
     else:
         return "<p>Login failed.</p>"
 
-@route('/restricted')
+@app.route('/restricted')
 def restricted_area():
     username = request.get_cookie("account", secret='some-secret-key')
     if username:
@@ -103,13 +107,18 @@ def restricted_area():
     else:
         return "You are not logged in. Access denied."
         
-@route('/counter')
+@app.route('/counter')
 def counter():
     count = int( request.cookies.get('counter', '0') )
     count += 1
     response.set_cookie('counter', str(count))
     return 'You visited this page %d times' % count
     
-    
-    
-run(host="localhost", port=8080, debug=True, reloader=True)
+
+if __name__ == '__main__':
+    run(app=app,
+        server='gunicorn',
+        host="localhost",
+        port=8080,
+        debug=True,
+        reloader=True)
